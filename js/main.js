8cbe1b4408a426d23f1f042cffe1cb0e06346fa4
Jonathan Braga <jonathanbraga@outlook.com>
2021-01-08 23:42:34 -0300

    Arrays e Dicionário

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ var idade = 38;
 var n1 = 10;
 var n2 = 6;
 
+var lista = ["maça","pêra","laranja"];
+
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}, {nome:"limão", cor:"verde"}];
+
 alert(nome +" tem "+ idade +" anos.");
 
 //para debugar no F12 do navegador
@@ -13,3 +17,23 @@ console.log(idade);
 //operações
 console.log("Soma: "+n1 + n2);
 console.log("Multiplicação: "+n1 * n2);
+
+//lendo arrays
+console.log(lista[1]);
+alert(lista[2]);
+
+//modificando arrays
+lista.push("uva");
+alert(lista);
+lista.pop();
+alert(lista);
+
+//convertendo
+console.log(lista);
+console.log(lista.toString()); //string
+console.log(lista.join(" | ")); //string com diferente separador
+
+//dicionário OO
+console.log(frutas);
+alert(frutas[1].nome);
+alert(frutas[2].cor);
