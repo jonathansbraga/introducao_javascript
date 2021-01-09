397310b4bdfb3d5328eb05bdd098e8a6883b550a
Jonathan Braga <jonathanbraga@outlook.com>
2021-01-08 23:20:29 -0300

    Fim aula 1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var nome = "Jonathan Braga";
+var idade = 38;
+
+var n1 = 10;
+var n2 = 6;
+
+alert(nome +" tem "+ idade +" anos.");
+
+//para debugar no F12 do navegador
+console.log(nome);
+console.log(idade);
+
+//operações
+console.log("Soma: "+n1 + n2);
+console.log("Multiplicação: "+n1 * n2);
